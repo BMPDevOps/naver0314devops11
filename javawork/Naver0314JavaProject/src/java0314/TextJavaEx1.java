42bc7a2a8a9a9a2102c245430caf9c43d45c04d5
BMDevOps <kumupaf6998@naver.com>
2024-03-14 15:44:05 +0900

    First Commit

---
+++ javawork/Naver0314JavaProject/src/java0314/TextJavaEx1.java
@@ -0,0 +1,10 @@
+package java0314;
+
+public class TextJavaEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("네이버 데브옵스 11기 개강날입니다.");
+	}
+
+}
