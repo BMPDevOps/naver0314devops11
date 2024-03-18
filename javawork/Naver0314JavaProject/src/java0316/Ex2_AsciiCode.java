6b384eda03b6d2f9d1dcd702fb1165e9c9ecebdb
BMDevOps <kumupaf6998@naver.com>
2024-03-18 11:40:43 +0900

    3월18일수업

---
+++ javawork/Naver0314JavaProject/src/java0316/Ex2_AsciiCode.java
@@ -0,0 +1,20 @@
+package java0316;
+
+public class Ex2_AsciiCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//char는 내부적으로 int 타입으로 인식
+		char a='A';
+		char b= 65; // 아스키 코드로 65는 문 'A'이
+		System.out.println(b);
+		
+		char c='7';
+		System.out.println(c);
+		
+		int n1 = c-'0';
+		
+		System.out.println(n1+2);
+	}
+
+}
---
+++ javawork/Naver0314JavaProject/src/java0316/Ex5_Switch.java
@@ -0,0 +1,9 @@
+package java0316;
+
+public class Ex5_Switch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
